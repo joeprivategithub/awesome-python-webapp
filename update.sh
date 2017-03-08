9f33674f765d7b2e175a6e95ee2908d11cf3d9cd
joe-pc <liuqs@ixdtech.com>
2017-03-08 14:41:12 +0800

    vbaIUtX3EAiBVndRRNlpuyVRamVNxDgPdfMKCKMS7TfF ## 03/08/2017 14:41:12

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm -f old_code/image_lib/image_new/*.png
-rm -f old_code/image_lib/image_new/*.jpg
-rm -f old_code/image_lib/image_spider/*.jpg
 commit_time=`date +%m/%d/%Y\ %H:%M:%S`
 commit_content=`openssl rand -base64 33`
 git add .
